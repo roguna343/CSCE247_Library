529993bd04ee33b65cdaab166c06b165845ae5fa
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-10-30 16:26:18 -0400

    Add files via upload

---
+++ People.java
@@ -0,0 +1,56 @@
+package CSCE247_Library;
+
+public abstract class People {
+	private String name;
+	private int age;
+	private String username;
+	private String password;
+	
+	public People() {
+		this.name = null;
+		this.age = 0;
+		this.username = null;
+		this.password = null;
+	}
+	
+	public People(String name, int age, String[] loginCred) {
+		this.name = null;
+		this.age = 0;
+		this.username = null;
+		this.password = null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public abstract String toString();
+}
