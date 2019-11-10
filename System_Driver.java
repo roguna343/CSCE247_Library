a00c389d2df662ee94627a8b3a7ee034d2d08b3b
JJ Satti <54640148+JJ8428@users.noreply.github.com>
2019-11-10 11:48:16 -0500

    Add files via upload

--- System_Driver.java
+++ System_Driver.java
@@ -1,5 +1,6 @@
 package Library247;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class System_Driver {
@@ -20,7 +21,34 @@ public class System_Driver {
 					+ "\n2. Sign up"
 					+ "\n3. Quit");
 			choice = Integer.parseInt(keyboard.nextLine());
-			
+			switch (choice) {
+				case 1:
+					break;
+				case 2:
+					System.out.println("Please enter the following parameters to create an account:");
+					System.out.println("Name");
+					String name = keyboard.nextLine();
+					System.out.println("Age:");
+					int age = Integer.parseInt(keyboard.nextLine());
+					System.out.println("Username:");
+					username = keyboard.nextLine();
+					System.out.println("Password:");
+					password = keyboard.nextLine();
+					LinkedList<String> checkedItem = new LinkedList<>();
+					LinkedList<String> lookForItem = new LinkedList<>();
+					System.out.println("Birthday:");
+					int birthday = Integer.parseInt(keyboard.nextLine());
+					System.out.println("Has Kids:");
+					boolean hasKids = Boolean.parseBoolean(keyboard.nextLine());
+					Adult toAdd = new Adult(name, age, username, password, checkedItem, lookForItem, birthday, 0, true, "", hasKids);
+					lib.addUser(toAdd);
+					lib.save();
+					continue;
+				default:
+					System.out.println("Exiting the program.");
+					System.exit(1);
+					break;
+			}
 		}
 		
 	}
