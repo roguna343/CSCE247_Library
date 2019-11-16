8169379f9383498d415a456fd278b9f7d91e6b79
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-11-16 13:11:39 -0500

    Add files via upload
    Covers testing of methods in the Library class. Which uses all methods developed in the code.

---
+++ LibraryTest.java
@@ -0,0 +1,104 @@
+package CSCE247_Library;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LibraryTest {
+
+	@Test
+	void testLibrary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetInv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUsr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUsr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsAvailable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPatronInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInventoryInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPushFines() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddNewItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddCopyOfItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+}
